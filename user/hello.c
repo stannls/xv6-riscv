c9bb1b48ef735f4dcdc7be9fa53cae501a2b7625
Yannis Storrer <yannis.storrer@web.de>
2026-04-22 18:25:30 +0200

    Userspace hello world

---
+++ user/hello.c
@@ -0,0 +1,11 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char** argv) {
+  if (argc <=1 ) {
+    fprintf(2, "Please enter a group name to greet.\n");
+    exit(1);
+  }
+  fprintf(1, "Hello Userspace, Group %s\n", argv[1]);
+  exit(0);
+}
